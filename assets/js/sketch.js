14d3a07f34355f0ef703a6560336e048bf721f44
Santiago Efrain Martinez Leon <60367880+Cholado@users.noreply.github.com>
2022-04-03 10:52:40 -0500

    Portfolio base

---
+++ assets/js/sketch.js
@@ -0,0 +1,73 @@
+let system;
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  system = new ParticleSystem(createVector(width / 2, height + 30));
+  filter(BLUR, 10);
+  c1 = color('#493c4d');
+  c2 = color('#29212b');
+}
+
+function draw() {
+  for(let y=0; y<height; y++){
+    n = map(y,0,height,0,1);
+    let newc = lerpColor(c1,c2,n);
+    stroke(newc);
+    line(0,y,width, y);
+  }
+  system.addParticle();
+  system.run();
+}
+
+// A simple Particle class
+let Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-4, 4), random(-8, 12));
+  this.position = position.copy();
+  this.lifespan = height / 3;
+};
+
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
+
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
+
+// Method to display
+Particle.prototype.display = function() {
+  stroke(246,239,187, this.lifespan);
+  strokeWeight(2);
+  fill(246,239,187, this.lifespan);
+  esize = random(6, 12);
+  ellipse(this.position.x, this.position.y, esize, esize);
+};
+
+// Is the particle still useful?
+Particle.prototype.isDead = function(){
+  return this.lifespan < 0;
+};
+
+let ParticleSystem = function(position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function() {
+  this.particles.push(new Particle(this.origin));
+};
+
+ParticleSystem.prototype.run = function() {
+  for (let i = this.particles.length-1; i >= 0; i--) {
+    let p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
