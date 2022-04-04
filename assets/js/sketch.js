89b38e3ed02b372c019eb5a8071760315d107151
Santiago Efrain Martinez Leon <60367880+Cholado@users.noreply.github.com>
2022-04-03 20:59:02 -0500

    re

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -21,7 +21,7 @@ function setup() {
 function draw() {
 	
 	let c = color("#15181F");
-	background(222, 10);
+	background('rgba(21,24,31,0.255)');
 	let yoff = 0;
 	let doCntMax = 300;
 	let doScale = 0.01;
