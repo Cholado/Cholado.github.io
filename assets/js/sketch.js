fb1aa0c8c7d147e333fac29bdc0a5cfa07a95ba0
Santiago Efrain Martinez Leon <60367880+Cholado@users.noreply.github.com>
2022-04-03 19:15:27 -0500

    Change sketch

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,73 +1,74 @@
-let system;
+const cell_n = 4;
+const line_n = 1000;
+const step = 0.01;
+const amplitude = 2.0;
+
+let cells = [];
+let points = [];
+let cell_size;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  system = new ParticleSystem(createVector(width / 2, height + 30));
-  filter(BLUR, 10);
-  c1 = color('#493c4d');
-  c2 = color('#ffffff');
+  background('white');
+  blendMode(SCREEN);
+
+	for (let i = 0; i < cell_n * width / height; i++) {
+		let cells_x = [];
+		for (let j = 0; j < cell_n; j++) {
+			cells_x.push({x: random(), y: random()});
+		}
+		cells.push(cells_x);
+	}
+
+	for (let i = 0; i < line_n; i++) {
+		points.push({x: random(width), y: random(height)});
+	}
 }
 
 function draw() {
-  background('white');
-  system.addParticle();
-  system.run();
+  cell_size = height / cell_n;
+	
+	for (let i = 0; i < line_n; i++) {
+		const hue = int(calculate_d(points[i].x, points[i].y) / cell_size * 400) % 360;
+
+		const v = curl(points[i].x, points[i].y);
+
+		stroke(color(`hsb(${hue}, 70%, 50%)`));
+		line(points[i].x, points[i].y, points[i].x + v.x, points[i].y + v.y);
+
+		points[i].x += v.x;
+		points[i].y += v.y;
+	}
+}
+
+function calculate_d(x, y) {
+	const xn = int(x / cell_size);
+	const yn = int(y / cell_size);
+	let min_d = 1e3;
+	for (let xx = max(-1, -xn); xx <= min(1, cells.length-xn-1); xx++) {
+		for (let yy = max(-1, -yn); yy <= min(1, cell_n-yn-1); yy++) {
+			const d = dist(
+				(xn + xx + cells[xn+xx][yn+yy].x) * cell_size,
+				(yn + yy + cells[xn+xx][yn+yy].y) * cell_size,
+				x,
+				y);
+			min_d = min(min_d, d);
+		}
+	}
+
+	return min_d;
+}
+
+function curl(x, y) {
+	const x1 = calculate_d(x + 1, y) / cell_size + noise((x + 1) * step, y * step) * amplitude;
+	const x2 = calculate_d(x - 1, y) / cell_size + noise((x - 1) * step, y * step) * amplitude;
+	const y1 = calculate_d(x, y + 1) / cell_size + noise(x * step, (y + 1) * step) * amplitude;
+	const y2 = calculate_d(x, y - 1) / cell_size + noise(x * step, (y - 1) * step) * amplitude;
+	
+	return {x: (y1 - y2) / 2 * cell_size, y: (x2 - x1) / 2 * cell_size};
 }
 
 function windowResized() {
   centerCanvas();
   resizeCanvas(windowWidth, windowHeight);
 }
-
-// A simple Particle class
-let Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-4, 4), random(-8, 12));
-  this.position = position.copy();
-  this.lifespan = height / 3;
-};
-
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function() {
-  stroke(199,177,205, this.lifespan);
-  strokeWeight(2);
-  fill(199,177,205, this.lifespan);
-  esize = random(6, 12);
-  ellipse(this.position.x, this.position.y, esize, esize);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  return this.lifespan < 0;
-};
-
-let ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
-    let p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
-    }
-  }
-};
