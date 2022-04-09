a4057b137c3e771de4280c4d382774a467e3fc3c
Santiago Efrain Martinez Leon <60367880+Cholado@users.noreply.github.com>
2022-04-09 08:02:09 -0500

    test grounds

---
+++ assets/js/tg.js
@@ -0,0 +1,45 @@
+var keyWord = "cholado";
+var nFramesInLoop = 120;
+var bEnableExport = false;
+var nElapsedFrames;
+var bRecording;
+var theCanvas;
+
+function setup() {
+    theCanvas = createCanvas(500, 200);
+    bRecording = false;
+    nElapsedFrames = 0;
+}
+
+function keyTyped() {
+if (bEnableExport) {
+    if ((key === 'r') || (key === 'R')) {
+    bRecording = true;
+    nElapsedFrames = 0;
+    }
+}
+}
+
+function draw() {
+    var percentCompleteFraction = 0;
+    if (bRecording) {
+      percentCompleteFraction = float(nElapsedFrames) / float(nFramesInLoop);
+    } else {
+      percentCompleteFraction = float(frameCount % nFramesInLoop) / float(nFramesInLoop);
+    }
+    renderMyDesign (percentCompleteFraction);
+    if (bRecording && bEnableExport) {
+      var frameOutputFilename = keyWord + "_frame_" + nf(nElapsedFrames, 4) + ".png";
+      print("Saving output image: " + frameOutputFilename);
+      saveCanvas(theCanvas, frameOutputFilename, 'png');
+      nElapsedFrames++;
+  
+      if (nElapsedFrames >= nFramesInLoop) {
+        bRecording = false;
+      }
+    }
+  }
+
+function renderMyDesign (percent) {
+
+}
